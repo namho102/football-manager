f5dfc0d6a4f4e1587e8f68013c6661374b7fdc76
Hoang Nam <namhoang.td09@gmail.com>
2016-10-04 12:55:46 +0700

    add SQL query

---
+++ SQLQuery/FBQuery1.sql
@@ -0,0 +1,52 @@
+alter trigger update_ranking on Fixtures
+after update
+as
+if(UPDATE(home_goal)) 
+begin
+	declare @home nchar(50)
+	declare @away nchar(50)
+	declare @hg int
+	declare @ag int
+	select @home=home, @away=away, @hg=home_goal, @ag=away_goal  from inserted
+
+	--update played game
+	update Ranking set pl = pl + 1 where team = @home or team = @away
+
+	--update home scores
+	update Ranking set f = f + @hg, a = a + @ag where team = @home
+	--update away scores
+	update Ranking set f = f + @ag, a = a + @hg where team = @away
+
+	--update goal difference
+	declare @gd int
+	set @gd = @hg - @ag
+	update Ranking set gd = gd + @gd where team = @home
+	update Ranking set gd = gd - @gd where team = @away
+	--win, lose or draw
+	if(@gd > 0)
+		begin
+			print 'home win'
+			update Ranking set pts = pts + 3, w = w + 1 where team = @home
+			update Ranking set l = l + 1 where team = @away
+		end	
+	else if(@gd = 0)
+		begin
+			print 'tie'
+			update Ranking set pts = pts + 1 where team = @home or team = @away
+			update Ranking set d = d + 1 where team = @home
+			update Ranking set d = d + 1 where team = @away
+		end
+	else
+		begin
+			print 'away win'
+			update Ranking set l = l + 1 where team = @home
+			update Ranking set pts = pts + 3, w = w + 1 where team = @away
+		end	
+		
+
+end	
+go
+
+update Fixtures set home_goal = 2, away_goal = 2  where id = 1
+insert into Fixtures values (1, 'Southampton',  NULL, NULL, 'Watford', '08-13-2016', '21:00:00')
+delete from Fixtures where id = 1
